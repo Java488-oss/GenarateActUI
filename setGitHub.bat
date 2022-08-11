add60c2cd029918dbc4714f01741622806b882ce
Java488-oss <job.mail1358@gmail.com>
2022-08-11 19:29:19 +0600

    test

--- setGitHub.bat
+++ setGitHub.bat
@@ -6,5 +6,5 @@ git add .
 set /p "id=Input commit: "
 git commit -m "%id%"
 git remote set-url origin https://Java488-oss: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@github.com/Java488-oss/GenarateActUI.git
-git push origin master
+git push origin main
 pause
